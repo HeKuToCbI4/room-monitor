bd10fc91cd134bea4468ee16def58cb1c5a764a0
Nikita <leonov2424@mail.ru>
2022-09-30 21:48:48 +0400

    This is for UART

--- stats.py
+++ stats.py
@@ -1,68 +1,8 @@
 import time
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-import subprocess
-
 import time
-import board
-import adafruit_dht
-import datetime
 from influxdb import InfluxDBClient
-
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D4)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
-
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=1, fill=1)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = -2
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = 0
-
-
-# Load default font.
-font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype('Minecraftia.ttf', 8)
+import serial
 
 ### INFLUX REGION
 
@@ -92,41 +32,44 @@ humidity_structure = {
     }
 }
 
+h2_ppm_structure = {
+    'measurement': 'h2_ppm',
+    'tags': {
+        'sensor': 'MQ-5',
+        'software_version': '0.01'
+    },
+    'time': None,
+    'fields': {
+        'value': None
+    }
+}
+
 ### END INFLUX REGION
 
-
-while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    try:
-        temperature_c = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-        timepoint = int(time.time())
-        time_precision='s'
-        temperature_structure['time'] = timepoint
-        temperature_structure['fields']['value'] = temperature_c
-        humidity_structure['time'] = timepoint
-        humidity_structure['fields']['value'] = humidity
-        influx_client.write_points([temperature_structure, humidity_structure], time_precision=time_precision)
-
-    except Exception as e:
-        print(e)
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 112500, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        try:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                print(f'Got the following data from arduino: {line}')
+                data = line.split('|')
+                if len(data) > 1:
+                    h2_ppm = float(data[0])
+                    temperature_c = float(data[1])
+                    humidity = float(data[2])
+                    timepoint = int(time.time())
+                    time_precision='s'
+                    temperature_structure['time'] = timepoint
+                    temperature_structure['fields']['value'] = temperature_c
+                    humidity_structure['time'] = timepoint
+                    humidity_structure['fields']['value'] = humidity
+                    h2_ppm_structure['time'] = timepoint
+                    h2_ppm_structure['fields']['value'] = h2_ppm
+                    influx_client.write_points([temperature_structure, humidity_structure, h2_ppm_structure], time_precision=time_precision)
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
         time.sleep(1)
-        continue
-    dht_string = "Temp: {:.1f} C\nHumidity: {}% ".format(
-                temperature_c, humidity
-            )
-    draw.text((0, 0),       f"IP: {IP.decode('utf-8')}",  font=font, fill=255)
-    draw.text((0, 10), 'Current sensor data:', font=font, fill = 255)
-    draw.text((0, 25),    str(dht_string),  font=font, fill=255)
-    draw.text((0, 50), f'{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}', font=font, fill=255)
-
-
-    # Display image.
-    disp.image(image)
-    disp.display()
-    time.sleep(10)
